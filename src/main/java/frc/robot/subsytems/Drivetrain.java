0de04b0cd093ce3359decf39f1499c31d8ca256a
Fritz Geib <fritz@standland.org>
2024-01-13 01:47:44 -0500

    add yagsl vendordep

---
+++ src/main/java/frc/robot/subsytems/Drivetrain.java
@@ -0,0 +1,16 @@
+package frc.robot.subsytems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+
+public class Drivetrain extends SubsystemBase {
+    private final SwerveDrive swerveDrive;
+
+    public double maxSpeed = 
+
+    public Drivetrain() {}
+
+    @Override
+    public void periodic() {
+    }
+}
