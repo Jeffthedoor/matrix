aecc125c38d663fbcd06460ffbc72c0787418c6d
Fritz Geib <fritz@standland.org>
2024-01-16 13:08:28 -0500

    remove some unnused and deprecated imports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,13 +3,6 @@ package frc.robot;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.numbers.N1;
-import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
 import swervelib.parser.PIDFConfig;
 
--- src/main/java/frc/robot/commands/AbsoluteDrive.java
+++ src/main/java/frc/robot/commands/AbsoluteDrive.java
@@ -1,26 +1,15 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
-import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.lib.shuffleboard.LightningShuffleboard;
 import frc.robot.subsystems.Drivetrain;
 
-import java.util.List;
 import java.util.function.DoubleSupplier;
 import swervelib.SwerveController;
-import swervelib.SwerveModule;
-import swervelib.math.SwerveMath;
 
 /**
  * An example command that uses an example subsystem.
