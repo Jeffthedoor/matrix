1e1d5b10ab5a1bc4f7950f16d8f178d8f3ea02ec
Fritz Geib <fritz@standland.org>
2024-01-16 13:08:00 -0500

    delete swervelib

--- src/main/java/frc/robot/lib/swervelib/AbsoluteEncoderFactory.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.lib.swervelib;
-
-@FunctionalInterface
-public interface AbsoluteEncoderFactory<Configuration> {
-    AbsoluteEncoder create(Configuration configuration);
-}
--- src/main/java/frc/robot/lib/swervelib/SwerveModuleFactoryBuilder.java
+++
@@ -1,4 +0,0 @@
-package frc.robot.lib.swervelib;
-
-public class SwerveModuleFactoryBuilder {
-}
