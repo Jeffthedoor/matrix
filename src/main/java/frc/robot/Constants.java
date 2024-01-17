47d8e042276a37114923f5bdb8a2a88e28ea0cf2
Fritz Geib <fritz@standland.org>
2024-01-17 01:39:06 -0500

    silly constany destruction

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import com.pathplanner.lib.util.PIDConstants;
 
 import edu.wpi.first.math.geometry.Rotation3d;
@@ -11,34 +9,14 @@ import edu.wpi.first.math.util.Units;
  * Class to hold all of the constants for the robot
  */
 public final class Constants {
-
-    // Spark max voltage compensation
-    public static final double VOLTAGE_COMPENSATION = 12d;
-
-    // Path to the blackout directory
-    public static final Path HURLEY_PATH = Paths.get("home/lvuser/Hurley");
-
-    // Check if we're on blackout
-    public static final boolean isHurley() {
-        return HURLEY_PATH.toFile().exists();
-    }
-
-    // Check if we're on Howitzer
-    public static final boolean isHowitzer() {
-        return !isHurley();
-    }
-
     // Constants for xbox controlers
     public static final class ControllerConstants {
         // Ports for the controllers
         public static final int DRIVER_CONTROLLER_PORT = 0;
         public static final int COPILOT_CONTROLLER_PORT = 1;
 
-        // Deadband, min, and max power for the controllers
+        // Deadband, for joysticks
         public static final double DEADBAND = 0.1d;
-        public static final double FLICKSTICK_DEADBAND = 0.15d;
-        public static final double MIN_POWER = 0d;
-        public static final double MAX_POWER = 1d;
     }
 
     // Constants for our drivetrain
@@ -50,7 +28,6 @@ public final class Constants {
         public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
 
         public static final double DRIVE_RATIO = 6.857142857142858;
-
 
         public static final double MAX_SPEED = Units.feetToMeters(14.4);
 
